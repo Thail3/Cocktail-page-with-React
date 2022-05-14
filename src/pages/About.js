d038fc54be2092a3fcea66e75fd1a530dad1c610
Thail3 <tanai.saeng@gmail.com>
2022-05-14 16:09:28 +0700

    finish

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+    </section>
+  );
+};
+
+export default About;
